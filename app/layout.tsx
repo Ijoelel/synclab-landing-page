35bfbf732ca6380e5b19fd26dd1a0e7f22418990
Ijoelel <fianarnatha13@gmail.com>
2025-06-04 03:04:58 +0700

    finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,36 @@
 import type { Metadata } from "next";
-import {Poppins} from "next/font/google"
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600'],
-  subsets: ['latin'], // Specify the character subsets you need, e.g., 'latin'
-  // display: 'swap', // Optional: for font display strategy
+    weight: ["100", "200", "300", "400", "500", "600"],
+    subsets: ["latin"], // Specify the character subsets you need, e.g., 'latin'
+    // display: 'swap', // Optional: for font display strategy
 });
 
 export const metadata: Metadata = {
-  title: "SaaS Landing Template",
-  description: "Landing Page template.",
+    title: "SyncLab | Get Premium Now",
+    description: "SyncLab Landing Page",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <head>
-        <script
-          defer
-          data-domain="saas.gonzalochale.dev"
-          src="https://plausible.gonzalochale.dev/js/script.outbound-links.js"
-        ></script>
-      </head>
-      <body
-        className={`${poppins.className} antialiased`}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <head>
+                <script
+                    defer
+                    data-domain="saas.gonzalochale.dev"
+                    src="https://plausible.gonzalochale.dev/js/script.outbound-links.js"
+                ></script>
+            </head>
+            <body className={`${poppins.className} antialiased`}>
+                <Providers>{children}</Providers>
+            </body>
+        </html>
+    );
 }
